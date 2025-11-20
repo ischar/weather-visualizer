115e0fd4186156a9c662413aa0462d9866f03255
Jaeho Cha <isitchar@gmail.com>
2025-11-20 01:46:21 +0000

    feat: setup project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import WeatherCard from './components/WeatherCard'
+import axios from 'axios';
+
+
+const App: React.FC = () => {
+  const [temp, setTemp] = useState<number>(0);
+  const today = new Date();
+  console.log(today);
+
+  return (
+    <div className="min-h-screen flex flex-col bg-white dark:bg-neutral-800">
+      <header className="h-15 mt-8 items-center text-center">
+        <div className="flex justify-center">
+          <p className="font-sans text-neutral-900 dark:text-neutral-100 text-3xl text-center">{temp}</p>
+          <p className="font-sans text-neutral-900 dark:text-neutral-100 text-xl">°</p>
+        </div>
+      </header>
+      <main>
+        <div>
+          <WeatherCard />
+        </div>
+      </main>
+      <footer>
+      </footer>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/components/WeatherCard.tsx
@@ -0,0 +1,11 @@
+function WeatherCard() {
+    return (
+        <div className="flex items-center w-2xl min-h-[220px] backdrop-opacity-40 rounded-xl mx-auto bg-neutral-50 dark:bg-neutral-950 items-center">
+            <div className="m-auto items-center bg-red-50 w-xl min-h-[100px]">
+                <p>sdfsdf</p>
+            </div>
+        </div>
+    )
+}
+
+export default WeatherCard;
